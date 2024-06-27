9fef0730be2ee794b9851882debb46493189f036
justabitcoiner <justabitcoiner@gmail.com>
2024-06-27 14:23:56 +0700

    tutorial - part 7

--- polls/models.py
+++ polls/models.py
@@ -2,6 +2,7 @@ import datetime
 
 from django.db import models
 from django.utils import timezone
+from django.contrib import admin
 
 
 # Create your models here.
@@ -12,6 +13,7 @@ class Question(models.Model):
     def __str__(self) -> str:
         return self.question_text
 
+    @admin.display(boolean=True, ordering="pub_date", description="Published recently?")
     def was_published_recently(self):
         now = timezone.now()
         days_ago = now - datetime.timedelta(days=1)

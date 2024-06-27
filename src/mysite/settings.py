861d716573157881cf16321190c9952457112126
justabitcoiner <justabitcoiner@gmail.com>
2024-06-27 23:07:57 +0700

    use .pgpass file to secure db password

--- src/mysite/settings.py
+++ src/mysite/settings.py
@@ -11,6 +11,9 @@ https://docs.djangoproject.com/en/5.0/ref/settings/
 """
 
 from pathlib import Path
+import os
+
+PGPASSFILE = os.getenv("PGPASSFILE")
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
@@ -85,9 +88,11 @@ DATABASES = {
         "ENGINE": "django.db.backends.postgresql",
         "NAME": "django",
         "USER": "postgres",
-        "PASSWORD": "postgres",
         "HOST": "127.0.0.1",
         "PORT": "5432",
+        "OPTIONS": {
+            "passfile": PGPASSFILE,
+        },
     }
 }
 
